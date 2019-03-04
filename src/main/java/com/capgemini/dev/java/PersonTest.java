001ce7ad741b6e815744481080018000ead3797f
suyash <suyash@suyash-PC.mshome.net>
2019-03-04 13:07:05 +0530

    Notes added

--- src/main/java/com/capgemini/dev/java/PersonTest.java
+++ src/main/java/com/capgemini/dev/java/PersonTest.java
@@ -3,18 +3,18 @@ package com.capgemini.dev.java;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class PersonTest {
-public static void main(String[] args) {
-	
-	AnnotationConfigApplicationContext ctx = 
-	                      new AnnotationConfigApplicationContext(PersonConfig.class);
-	
-	Person p = ctx.getBean(Person.class);
-	System.out.println(p.getMailid());
-	System.out.println(p.getName());
-	p.getJob().dowork();
-	
-	ctx.close();
-	
-	
-}
+	public static void main(String[] args) {
+
+		AnnotationConfigApplicationContext ctx = 
+				new AnnotationConfigApplicationContext(PersonConfig.class);
+
+		Person p = ctx.getBean(Person.class);
+		System.out.println(p.getMailid());
+		System.out.println(p.getName());
+		p.getJob().dowork();
+
+		ctx.close();
+
+
+	}
 }
